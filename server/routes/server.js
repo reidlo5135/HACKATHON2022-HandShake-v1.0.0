5052949e7dfaab443bb7b99a93b7b07a9054f635
reidlo5135 <201840103@email.daelim.ac.kr>
2022-06-24 19:02:35 +0900

    06/24 19:01 CHORE: vc
    SUCCESS

--- server/routes/server.js
+++ server/routes/server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('mysql');
+const dbinfo = require('../env/dbEnv');
+const connection = mysql.createConnection(dbinfo);
 
-router.get('/api/test', (req, res) => {
-   res.send({'data':'HI!!!!!!'});
+router.get('/api/db/test', (req, res) => {
+   connection.query('SELECT * FROM testTable', (error, rows, fields) => {
+      if(error) throw error;
+      console.log('TEST DB INFO rows : ' + JSON.stringify(rows));
+      console.log('TEST DB INFO fields : ' + JSON.stringify(fields));
+      res.send(rows);
+   });
 });
 
 module.exports = router;

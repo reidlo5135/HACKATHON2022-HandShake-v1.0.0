4286cac31ffe2391409d0cc10f350bfcea5a8272
dongho <ehdrh321@gmail.com>
2022-06-24 16:23:24 +0900

    06/24 16:23 Refactor : Select.js class 오타 수정

--- client/src/component/Select.js
+++ client/src/component/Select.js
@@ -26,54 +26,54 @@ export default function Select(){
             exit={{ opacity: 0 }}
             transition={{ ease: "easeOut", duration: 1 }}
         >
-            <div class="select-tit">
+            <div className="select-tit">
                 SELECT
             </div>
 
-            <div class="select">
-                <div class="left-content">
+            <div className="select">
+                <div className="left-content">
                     <div className='one'>
                         <span>A</span>
-                        <div class="campus btn-selcet">
+                        <div className="campus btn-selcet">
                             <img src={Campus} alt="캠퍼스 아이콘" /> 
                         </div>
                     </div>
 
                     <div className='two'>
                         <span>B</span>
-                        <div class="food btn-selcet">
+                        <div className="food btn-selcet">
                             <img src={Food} alt="오늘의 메뉴 아이콘"/>  
                         </div>
                     </div>
 
                     <div className='three'>
                         <span>C</span>
-                        <div class="calender btn-selcet">
+                        <div className="calender btn-selcet">
                             <img src={Calender} alt="학사 일정 아이콘"/> 
                         </div>
                     </div>
 
                     <div className='three'>
                         <span>D</span>
-                        <div class="facility btn-selcet">
+                        <div className="facility btn-selcet">
                             <img src={Food} alt="오늘의 메뉴 아이콘"/> 
                         </div>
                         </div>
                     </div>
                 
-                <div class="square">
-                    <div class="square1"></div>
-                    <div class="square2"></div>
-                    <div class="square3"></div>
+                <div className="square">
+                    <div className="square1"></div>
+                    <div className="square2"></div>
+                    <div className="square3"></div>
                 </div>
 
-                <div class="right-content">
-                    <div class="cam">
+                <div className="right-content">
+                    <div className="cam">
 
                     </div>
                 </div>  
             </div>
-            <div class="tuto-desc">
+            <div className="tuto-desc">
                 <span>캠 화면을 통해 수어를 인식합니다</span><br/>
                 <span>수어로 선택해 주세요!</span>
             </div>
